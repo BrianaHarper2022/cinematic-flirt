122010fdf266a46db2f5fdb8d8776a123f33a8f3
Briana Harper <williamsbriana2022@gmail.com>
2024-07-06 16:58:37 -0400

    Fixed navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,10 +3,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Navbar() {
     return (
-        <div classNameName="Navbar container">
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Cinematic Flirt</a>
+                    <a className="navbar-brand" href="/">Cinematic Flirt</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -16,18 +15,17 @@ export default function Navbar() {
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Support</a>
+                            <a className="nav-link" href="/">Support</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
+                            <a className="nav-link" href="/">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
+                            <a className="nav-link" href="/">Contact</a>
                         </li>
                     </ul>
                 </div>
                 </div>
-            </nav>    
-        </div>
+            </nav>
     )
 }
