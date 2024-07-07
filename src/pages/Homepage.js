c329438e5cec53bc08c9c1b3de4b68a39fdd3496
Briana Harper <williamsbriana2022@gmail.com>
2024-07-06 21:34:56 -0400

    Added responsive CSS

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,6 +4,7 @@ import "../styles/Homepage.css";
 export default function Homepage() {
     return (
         <div className="Homepage">
+            <h3>Cinematic Flirt</h3>
             <h1>Connect based on your favorite movies</h1>
             <h2>Match anywhere. Watch anytime</h2>
             <a href="#signin" className="mb-5">Sign In</a>
