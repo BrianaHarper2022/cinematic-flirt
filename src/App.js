6cf3eb801d8b04ba62d4c5a594f4806d286e1535
Briana Harper <williamsbriana2022@gmail.com>
2024-07-06 20:15:58 -0400

    Set up basic routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Navbar from "./pages/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from "./pages/Homepage";
 import SignIn from "./pages/SignIn";
 import Footer from "./pages/Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Support from "./pages/Support";
+import Layout from "./Layout";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-      <main>
-        <Homepage />
-        <SignIn />
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<Layout/>}>
+        <Route path="/Homepage" element={<Homepage/>}/>
+        <Route path="/SignIn" element={<SignIn/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Support" element={<Support/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+        <Route path="/Footer" element={<Footer/>}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,31 +1,21 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-            <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Cinematic Flirt</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Support</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                </div>
-            </nav>
+    <div>
+        <nav>
+            <ul>
+        <li><Link to="/Homepage">Homepage</Link></li>
+        <li><Link to="/SignIn">SignIn</Link></li>
+        <li><Link to="/About">About</Link></li>
+        <li><Link to="/Support">Support</Link></li>
+        <li><Link to="/Contact">Contact</Link></li>
+        <li><Link to="/Footer">Footer</Link></li>
+        
+        </ul>
+        </nav>
+    </div>
     )
 }
