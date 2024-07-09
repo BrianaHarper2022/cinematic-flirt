827247c1c41efd85b3174f804094344c79fccdeb
Briana Harper <williamsbriana2022@gmail.com>
2024-07-09 04:37:14 -0400

    Enhance Contact page styling and functionality
    - Update Contact component to include form submission handling
    
    - Refine CSS for Contact form elements, improving layout and responsiveness

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "../styles/Contact.css";
 
 export default function Contact() {
+    function handleSubmit(event) {
+        event.preventDefault();
+        alert("Thank you for contacting us. We will contact you shortly!");
+    }
+
     return (
-        <div>
-            <h4>This is Contact page</h4>
+        <div className="contact">
+            <h1 className="m-4 text-center">Contact Us</h1>
+            <h4 className="text-center">Online Inquiry</h4>
+            <form>
+                <input className="text" type="text" placeholder="Name" required/>
+                <input className="email" type="email" placeholder="Email" required/>
+                <input className="tel" type="tel" placeholder="Phone Number" required/>
+                <textarea className="message" name="message" cols="30" rows="10" value="" placeholder="How Can We Help?" required></textarea>
+                <button className="contact-button" onClick={handleSubmit}>Submit</button>
+            </form>
         </div>
-    )
+    );
 }
