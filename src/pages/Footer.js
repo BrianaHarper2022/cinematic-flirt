78b7776e14ed0a285f5bdf85a1adf3c94c424e75
Briana Harper <williamsbriana2022@gmail.com>
2024-07-08 22:40:51 -0400

    Added Footer css

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/Footer.css";
 
 export default function Footer() {
     return (
-        <div>
+        <div className="Footer">
             <p>This project was built by <a href="https://briana-harper-portfolio.netlify.app" target="_blank"
             rel="noopener noreferrer">Briana Harper</a> and is <a href="https://github.com/BrianaHarper2022?tab=repositories" target="_blank"
             rel="noopener noreferrer">open-sourced on Github</a> and hosted on <a href="https://cinematic-flirt.netlify.app/" target="_blank"
