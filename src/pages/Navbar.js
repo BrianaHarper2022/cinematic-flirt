8b1534e83303cb69f7d6b0194af202af4e9fc855
Briana Harper <williamsbriana2022@gmail.com>
2024-07-09 04:19:13 -0400

    Adjust styling for navbar links and logo

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -5,7 +5,6 @@ import "../styles/Navbar.css";
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     
-
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
@@ -15,10 +14,10 @@ export default function Navbar() {
           <div className="navbar-container">
             <ul className={isOpen ? 'nav-links open' : 'nav-links'}>
               <li className="logo">Cinematic Flirt</li>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/Support">Support</Link></li>
-              <li><Link to="/About">About</Link></li>
-              <li><Link to="/Contact">Contact</Link></li>
+              <li className="link"><Link to="/">Home</Link></li>
+              <li className="link"><Link to="/Support">Support</Link></li>
+              <li className="link"><Link to="/About">About</Link></li>
+              <li className="link"><Link to="/Contact">Contact</Link></li>
             </ul>
             <div className="hamburger" onClick={toggleMenu}>
               &#9776; 
