0c18e64c1aa0a3b48915abd4f96d7b5a2d1b5e04
Briana Harper <williamsbriana2022@gmail.com>
2024-07-17 17:44:32 -0400

    Refactor SignIn component and CSS for improved structure and styling
    - Unified SignIn component into a single structure with consistent styling
    - Applied Bootstrap classes for responsive design and form elements
    - Updated CSS for SignIn component to ensure cohesive styling and layout
    - Enhanced usability and visual consistency of the SignIn form

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -9,22 +9,26 @@ export default function SignIn() {
     }
 
     return (
-        <div className="SignIn">
-            <div className="box">
-                        <h2>Sign In</h2>
-                <form>
-                    <div>
-                        <input className="user-info" type="text" placeholder="Email" />
-                        <input className="password" type="password" placeholder="Password" minlength="8" required/>
-                        <input type="checkbox" name="remember-me" id="remember-me" defaultChecked />
-                        <label className="remember-me" htmlFor="remember-me">Remember Me</label>
-                    <div>
-                        <input onClick={handleSignIn} className="submit-button" type="submit" value="Sign In" />
-                    </div>
-                    </div>
-                </form>
-                <p>Forgot password?</p>
-                <p className="Sign-up-link">New to Cinematic Flirt? Sign up now.</p>
+        <div className="SignIn-container">
+            <div className="box row justify-content-center">
+                <div className="col">
+                    <h2>Sign In</h2>
+                        <form>
+                            <div>
+                                <input className="user-info" type="text" placeholder="Email" required/>
+                                <input className="password" type="password" placeholder="Password" minLength="8" required/>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" name="remember-me" id="remember-me" defaultChecked />
+                                <label className="form-check-label" htmlFor="remember-me">Remember Me</label>
+                            </div>
+                            <div>
+                                <button type="button" className="btn btn-light submit-button" onClick={handleSignIn}>Sign In</button>
+                            </div>
+                        </form>
+                                <p>Forgot password?</p>
+                                <p className="Sign-up-link">New to Cinematic Flirt? Sign up now.</p>
+                </div>     
             </div>
         </div>
     )
