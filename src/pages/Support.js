ae195f6ea528f186c78a43652355cba84307a512
Briana Harper <williamsbriana2022@gmail.com>
2024-08-06 17:07:38 -0400

    Updated Support page content and styling

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Support.css";
+import { BsSearchHeart } from "react-icons/bs";
+
 
 export default function Support() {
     return (
         <div>
-            <h4>This is Support page</h4>
+            <div className="support-hero">
+                <h1 className="text-center">Hi. How can we help?</h1>
+            <div className="support-search-input-container">
+                <BsSearchHeart className="support-search-icon"/>
+                <input className="support-search" type="search" name="query" id="query" placeholder="Search" autoComplete="off" aria-label="Search"></input>
+            </div>
+            </div>
+            <div className="d-flex justify-content-center">
+                <ul>
+                    <li className="support-link m-3"><Link to="/Contact">Getting Started</Link></li>
+                    <li className="support-link m-3"><Link to="/Contact">Our Guidelines</Link></li>
+                    <li className="support-link m-3"><Link to="/Contact">Privacy & Safety</Link></li>
+                    <li className="support-link m-3"><Link to="/Contact">Contact Us</Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
