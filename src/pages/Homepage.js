9e88b7804b443e9441d3572ce0a2a371c181d47c
Briana Harper <williamsbriana2022@gmail.com>
2024-08-29 19:53:55 -0400

    Updated homepage button for consistency and readability

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
-import SignInButton from "./SignInButton";
-import "../styles/Homepage.css";
+import HomepageButton from "./HomepageButton";
 import Homepage2 from "./Homepage2";
+import "../styles/Homepage.css";
 
 export default function Homepage() {
     return (
@@ -11,7 +11,7 @@ export default function Homepage() {
                     <h3>Cinematic Flirt</h3>
                     <h1>Connect based on your favorite movies</h1>
                     <h2>Match anywhere. Watch anytime</h2>
-                    <SignInButton />
+                    <HomepageButton />
                 </div>
             </header>
                 <div>
